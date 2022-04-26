d4b933dc8dc42f0a7e3e6e0de77bbe78c304bf16
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-26 16:27:56 -0300

    Tentativa Desafio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 // import { getCategories } from './services/api';
 
-function App() {
-  return (
-    <title />);
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
