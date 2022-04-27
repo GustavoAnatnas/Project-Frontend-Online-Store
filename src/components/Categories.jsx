01d6b81e504ff4df0ea30346ddd700e343b3b1e2
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-26 21:25:40 -0300

    Desafio 6

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,31 +1,69 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Categories extends Component {
   constructor() {
     super();
     this.state = {
       categories: [],
+      selected: '',
+      products: [],
     };
   }
 
   async componentDidMount() {
     const fetchCategories = await getCategories();
     const categoriesNames = fetchCategories.map((element) => (
-      <li data-testid="category" key={ element.id }>{ element.name }</li>));
+      <div key={ element.id }>
+        <label htmlFor={ element.id }>
+          <input
+            value={ element.id }
+            id={ element.id }
+            type="radio"
+            name="category"
+            data-testid="category"
+            onClick={ this.onChecked }
+          />
+          { element.name }
+        </label>
+      </div>));
     this.setState({
       categories: categoriesNames,
     });
   }
 
-  render() {
-    const { categories } = this.state;
-    return (
-      <div>
-        <ul>
-          {categories}
-        </ul>
-      </div>
-    );
-  }
+onChecked = ({ target }) => {
+  const { value } = target;
+
+  this.setState({
+    selected: value,
+  });
+  this.fetchProductsByCategories();
+}
+
+fetchProductsByCategories = async () => {
+  const { selected } = this.state;
+  const fetchProducts = await getProductsFromCategoryAndQuery(selected);
+  // console.log(fetchProducts);
+  const resultsProducts = fetchProducts.results.map((product) => (
+    <div data-testid="product" key={ product.id }>
+      <h3>{product.title}</h3>
+      <img src={ product.thumbnail } alt={ product.title } />
+      <p>{`R$:${product.price}`}</p>
+    </div>
+  ));
+  this.setState({
+    products: resultsProducts,
+  });
+}
+
+render() {
+  const { categories, products } = this.state;
+  return (
+    <div>
+      {categories}
+      {products}
+    </div>
+  );
+}
 }
