6d9a255b14d89c4341658a8cbb9184a44028b10e
BrenoBTrindade <brenokaminski@hotmail.com>
2022-04-27 17:05:31 -0300

    requisito 7 concluido

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -1,30 +1,40 @@
 import React, { Component } from 'react';
-// import { getProductDetails } from '../services/api';
+import { getProductDetails } from '../services/api';
 
 export default class ProductCard extends Component {
   constructor() {
     super();
     this.state = {
       details: {},
+      attributes: [],
     };
   }
 
-  // async componentDidMount() {
-  //   // const { match: { params: { id } } } = this.props;
-  //   const result = await getProductDetails(id);
-  //   this.state({
-  //     details: result,
-  //   });
-  // }
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const result = await getProductDetails(id);
+    const attribute = result.attributes.map((attr) => (
+      <div key={ attr.id }>
+        <p>{`${attr.name} : ${attr.value_name}`}</p>
+      </div>
+    ));
+    this.setState({
+      details: result,
+      attributes: attribute,
+    });
+  }
 
   render() {
-    const { details } = this.state;
+    const { details, attributes } = this.state;
     return (
       <div>
         <div data-testid="product" key={ details.id }>
           <h3 data-testid="product-detail-name">{details.title}</h3>
           <img src={ details.thumbnail } alt={ details.title } />
           <p>{`R$:${details.price}`}</p>
+          <aside>
+            {attributes}
+          </aside>
         </div>
       </div>
     );
